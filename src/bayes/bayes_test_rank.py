749c870c2e06be3c77ca002bf7035fa3fbf71c73
Maxim <maksukh@inbox.ru>
2021-12-14 05:36:46 +0300

    Use logarithms for Bayes

--- src/bayes/bayes_test_rank.py
+++ src/bayes/bayes_test_rank.py
@@ -1,26 +1,30 @@
+import numpy as np
+
 from src.shared.base_models import TestOccurrencesRank
 
 
 class BayesTestRank(TestOccurrencesRank):
     def key(self, test, test_info):
         test_name = test["name"]
-        if test_info.num_failed.get(test_name, 0) == 0:
-            return 1
         if test_info.num_runs.get(test_name, 0) == 0:
-            return 0
+            return -np.inf
+        if test_info.num_failed.get(test_name, 0) == 0:
+            return np.inf
 
         runs = test_info.num_runs[test_name]
         freq_fail = test_info.num_failed[test_name] / runs
-        alpha = 10
+        alpha = 1
 
-        prod_changed = 1
+        prod_changed_log = 0
         for filename in test_info.changed_files:
             key = (filename, test_name)
-            prod_changed *= test_info.file_changed_test_failed.get(key, 0) + alpha * freq_fail
-            prod_changed /= test_info.num_failed[test_name] + alpha
+            changed_and_failed = test_info.file_changed_test_failed.get(key, 0)
+            prod_changed_log += np.log(changed_and_failed + alpha * freq_fail)
 
-        p_fail = freq_fail * prod_changed
-        return 1 - p_fail
+        prod_changed_log -= len(test_info.changed_files) * np.log(test_info.num_failed[test_name] + alpha)
+
+        p_fail = np.log(freq_fail) + prod_changed_log
+        return -p_fail
 
     def rank(self, test_occurrences, test_info):
         return sorted(test_occurrences, key=lambda test: self.key(test, test_info))
